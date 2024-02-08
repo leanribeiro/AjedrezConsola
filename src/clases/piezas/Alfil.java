ade6c3289ad81e344075c8fe2a8516dd15badd90
leanribeiro <ribeiro.leandromanuel@gmail.com>
2024-02-08 10:59:20 +0100

    Formateo clase alfil

--- src/clases/piezas/Alfil.java
+++ src/clases/piezas/Alfil.java
@@ -8,10 +8,11 @@ import java.util.List;
 
 public class Alfil extends Pieza {
 
-    public Alfil(int posicion_x, int posicion_y){
+    public Alfil(int posicion_x, int posicion_y) {
         super.setPosicion_x(posicion_x);
         super.setPosicion_y(posicion_y);
     }
+
     public Alfil(String nombre, String color, int posicion_x, int posicion_y) {
         super(nombre, color, posicion_x, posicion_y);
     }
@@ -28,11 +29,11 @@ public class Alfil extends Pieza {
         int posicionYActual = super.getPosicion_y();
 
         boolean salirAbajoDerecha = false;
-        for(int x=0; x<8 && !salirAbajoDerecha ;x++){
-            if((posicionXActual +x) <8  && (posicionYActual+x) <8){
-                if (tablero.validarPosicionAMover(posicionXActual+ x +1, posicionYActual+x +1)) {
-                    movimientos.add(String.valueOf((posicionXActual+x+1)) + String.valueOf(posicionYActual+x+1));
-                }else{
+        for (int x = 0; x < 8 && !salirAbajoDerecha; x++) {
+            if ((posicionXActual + x) < 8 && (posicionYActual + x) < 8) {
+                if (tablero.validarPosicionAMover(posicionXActual + x + 1, posicionYActual + x + 1)) {
+                    movimientos.add(String.valueOf((posicionXActual + x + 1)) + String.valueOf(posicionYActual + x + 1));
+                } else {
                     salirAbajoDerecha = true;
                 }
             }
@@ -40,18 +41,18 @@ public class Alfil extends Pieza {
         boolean salirAbajoIzq = false;
         for (int x = 0; ((posicionXActual + x) < tablero.getTablero().length || (posicionYActual - x) >= 0)
                 && !salirAbajoIzq; x++) {
-            if (tablero.validarPosicionAMover(posicionXActual+x+1, posicionYActual - x -1)) {
-                movimientos.add(String.valueOf((posicionXActual+x +1 )) + String.valueOf(posicionYActual - x -1));
-            }else{
+            if (tablero.validarPosicionAMover(posicionXActual + x + 1, posicionYActual - x - 1)) {
+                movimientos.add(String.valueOf((posicionXActual + x + 1)) + String.valueOf(posicionYActual - x - 1));
+            } else {
                 salirAbajoIzq = true;
             }
         }
         boolean salirArribaIzq = false;
         for (int x = 0; ((posicionXActual - x) >= 0 && (posicionYActual - x) >= 0)
                 && !salirArribaIzq; x++) {
-            if (tablero.validarPosicionAMover(posicionXActual - x -1 , posicionYActual - x -1 )) {
-                movimientos.add(String.valueOf((posicionXActual-x -1)) + String.valueOf(posicionYActual - x -1));
-            }else{
+            if (tablero.validarPosicionAMover(posicionXActual - x - 1, posicionYActual - x - 1)) {
+                movimientos.add(String.valueOf((posicionXActual - x - 1)) + String.valueOf(posicionYActual - x - 1));
+            } else {
                 salirArribaIzq = true;
             }
         }
@@ -59,15 +60,15 @@ public class Alfil extends Pieza {
         boolean salirArribaDer = false;
         for (int x = 0; ((posicionXActual - x) >= 0 && (posicionYActual + x) < tablero.getTablero()[0].length)
                 && !salirArribaDer; x++) {
-            if (tablero.validarPosicionAMover(posicionXActual - x -1 , posicionYActual + x +1)) {
-                movimientos.add(String.valueOf((posicionXActual-x -1)) + String.valueOf(posicionYActual + x +1));
-            }else{
+            if (tablero.validarPosicionAMover(posicionXActual - x - 1, posicionYActual + x + 1)) {
+                movimientos.add(String.valueOf((posicionXActual - x - 1)) + String.valueOf(posicionYActual + x + 1));
+            } else {
                 salirArribaDer = true;
             }
         }
 
         return movimientos;
-}
+    }
 
     @Override
     public void movimiento(Pieza pieza, Tablero tablero) {
@@ -85,4 +86,4 @@ public class Alfil extends Pieza {
     }
 
 
-   }
+}
